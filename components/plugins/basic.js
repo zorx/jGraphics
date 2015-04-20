dc3704c24cab461acea7f3437276fd57ec749193
Sacha WILT <sacha.wilt@gmail.com>
2015-04-20 14:33:15 +0200

    refactor(basic): comments and renaming

--- components/plugins/basic.js
+++ components/plugins/basic.js
@@ -1,31 +1,62 @@
-/* register Basic Elements with default args */
-var listElements = [
-  {name:"circle", args:[{cx:0}, {cy:0}, {r:5}], path: {x: "cx", y: "cy"}},
-  {name:"rect", args:[{x:0}, {y:0}, {width:100}, {height:100}], path: {x: "x", y: "y"}},
-  {name:"ellipse", args:[{cx:0}, {cy:0}, {rx:10}, {ry:10}], path: {x: "cx", y: "cy"}},
-  {name:"image", args:[{"xlink:href":"#"}, {x:0}, {y:10}], path: {x: "x", y: "y"}},
-  {name:"line", args:[{x1:0}, {y1:0}, {x2:10}, {y2:10}], path: {x: ["x1", "x2"], y: ["y1", "y2"]}}
+// Register basic canvas elements with their respective args
+// @TODO: "path" is actually "position"
+var elements = [
+  {
+    name: "circle",
+    args: [{cx:0}, {cy:0}, {r:5}],
+    path: {x: "cx", y: "cy"}
+  },
+  {
+    name: "rect",
+    args: [{x:0}, {y:0}, {width:100}, {height:100}],
+    path: {x: "x", y: "y"}
+  },
+  {
+    name: "ellipse",
+    args: [{cx:0}, {cy:0}, {rx:10}, {ry:10}],
+    path: {x: "cx", y: "cy"}
+  },
+  {
+    name: "image",
+    args: [{"xlink:href":"#"}, {x:0}, {y:10}],
+    path: {x: "x", y: "y"}
+  },
+  {
+    name: "line",
+    args: [{x1:0}, {y1:0}, {x2:10}, {y2:10}],
+    path: {x: ["x1", "x2"], y: ["y1", "y2"]}
+  }
 ];
 
-$.each(listElements, function(key, element) {
-  // Circle plugin (based on Arc)
+// Register each element through a plugin
+$.each(elements, function(index, element) {
   $.jGraphics.registerPlugin({
     name: element.name,
     path: element.path,
-    construct: function(args) {
+
+    // How to construct future plugin instance
+    construct: function (args) {
       var self = this;
-      $.each(element.args, function (key, defaultArgs) {
+      // Arguments is an array, not an object (accessed by indexes)
+      $.each(element.args, function (index, defaultArgs) {
         $.each(defaultArgs, function(argName, argDefaultValue) {
-          self[argName] = args[key] || element.args[key][argName];
+          // Assigning object default attributes value (~extend)
+          self[argName] = args[index] || element.args[index][argName];
         });
       });
     },
-    render: function(self) {
-      var svgElement = document.createElementNS("http://www.w3.org/2000/svg",element.name);
-      $.each(self.args, function(key, value) {
-        svgElement.setAttribute(key, value);
+
+    // Display element instance
+    render: function (instance) {
+      // Create SVG element
+      var svgElement = document.createElementNS("http://www.w3.org/2000/svg", element.name);
+      // Assign arguments as attributes (position, size, etc.)
+      $.each(instance.args, function(index, value) {
+        svgElement.setAttribute(index, value);
       });
-      self.attr(self.args);
+      // Apply attributes to DOM
+      instance.attr(instance.args);
+      // Returning SVG element to show
       return svgElement;
     }
   });
